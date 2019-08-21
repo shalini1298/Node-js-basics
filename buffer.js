fed9bb19c05ebe8b587fffb8554b2d7a2126d646
shalini <48598782+shalini1298@users.noreply.github.com>
2019-08-21 17:51:08 +0530

    Update buffer.js

--- buffer.js
+++ buffer.js
@@ -1,16 +1,10 @@
 const buf1 = Buffer.alloc(10);
 console.log(buf1);
 
-// Creates a Buffer of length 10, filled with 0x1.
 const buf2 = Buffer.alloc(10, 1);
 console.log(buf2);
-// Creates an uninitialized buffer of length 10.
-// This is faster than calling Buffer.alloc() but the returned
-// Buffer instance might contain old data that needs to be
-// overwritten using either fill() or write().
 const buf3 = Buffer.allocUnsafe(10);
 console.log(buf3);
 
-// Creates a Buffer containing [0x1, 0x2, 0x3].
 const buf4 = Buffer.from([1, 2, 3]);
 console.log(buf4);
